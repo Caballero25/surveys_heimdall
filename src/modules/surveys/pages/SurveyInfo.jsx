fc91cfef3316a78929cb4afb1956099c05260237
David Caballero <davidcame124@gmail.com>
2025-09-01 00:19:14 -0500

    add options a modulo D

--- src/modules/surveys/pages/SurveyInfo.jsx
+++ src/modules/surveys/pages/SurveyInfo.jsx
@@ -38,6 +38,7 @@ export default function SurveyInfo() {
       "Completacion Quimica - Configuracion",
       "Monitoreo Consumo - trazabilidad",
       "Historico TearDown",
+      "Cros-plot Comparativo de Resultados"
     ],
   };
 
